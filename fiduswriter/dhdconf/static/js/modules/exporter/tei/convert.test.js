neuged/fiduswriter-dhdconf

commit 22988c15098fdbe731732bd75ef03e398d075f1f

TEI Export: Adjust citation and bibliography handling
* Show citation texts in the document export
* Adjust bibliography to more closely match dhconvalidator output

fiduswriter/dhdconf/static/js/modules/exporter/tei/convert.js
@@ -7,13 +7,12 @@
  * @module
  */
 
-import extract from "./extract"
-import {tag, wrap} from "./utils"
+import extract, {extractTextNodes} from "./extract"
+import {tag, wrap, linkify} from "./utils"
 import {header} from "./templates/header"
 import {body} from "./templates/body"
 import {back} from "./templates/back"
 import {TEITemplate} from "./templates"
-import bibliography from "./bibliography"
 
 
 function authors(data) {
@@ -40,27 +39,31 @@ function keywords(data) {
  */
 function text(item) {
     if (item.marks && item.marks.length) {
-        const result = item.marks.reduce((previous, current) => {
+        return item.marks.reduce((previous, current) => {
             if (current.type === 'em') {
                 return wrap('hi', previous, {rend: 'italic'})
             } else if (current.type === 'strong') {
                 return wrap('hi', previous, {rend: 'bold'})
+            } else if (current.type === 'link') {
+                // TODO Handle links
             }
             return previous
         }, item.text)
-        return result
     }
     return item.text
 }
 
 /**
- * Build a string of TEI from the content of a part of docContents
- * with type 'richtext'.
+ * Build a string of TEI from the body (type 'richtext') of a document.
+ * Returns a string representing as the body and a second string having
+ * the footnotes.
  */
-function richText(richTextContent, imgDB) {
+function convertBody(richTextContent, imgDB, citationTexts) {
     let divLevel = 0    // the number of currently open divs
     let fnCount = 0     // the number of footnotes we have encountered
-    let figCount = 0  // the number of figures we have encountered
+    let figCount = 0    // the number of figures we have encountered
+    let citeCount = 0   // the number of citations we have encountered
+    let footnotesTEI = []
 
     function f(item) {
         /* This is a base case because we have arrived at a leaf node. */
@@ -74,10 +77,13 @@ function richText(richTextContent, imgDB) {
         /* Another base case, since the actual content of the footnote
          * is only needed at the bottom of the text. */
         if (item.type === 'footnote') {
-            // This only handles the markup for footnotes inside the
-            // regular text. The actual footnotes have to be generated
-            // separately and placed at the bottom of the text.
-            fnCount++
+            fnCount += 1
+            footnotesTEI.push(wrap(
+                'note',
+                item.attrs.footnote.map(c => f(c)).join(''),
+                {n: fnCount, rend: 'footnote text', 'xml:id': `ftn${fnCount}`}
+            ))
+            // Return only the markup for footnotes inside the regular text.
             return tag('ref', {n: fnCount, target: `ftn${fnCount}`})
         }
 
@@ -185,23 +191,25 @@ function richText(richTextContent, imgDB) {
             return `${closing}${opening}${head}`
         }
 
-        // console.log(`Could not parse richtextContent of type ${item.type}`)
+        if (item.type === 'citation') {
+            return citationTexts[citeCount++]
+        }
+
         return ''
     }
 
     const result = richTextContent.map(c => f(c)).join('')
     const closing = '</div>'.repeat(divLevel)
+    const body = `${result}${closing}`
 
-    return `${result}${closing}`
+    const footnotes = wrap('div', footnotesTEI.join("\n"), {type: 'notes'})
+    return [body, footnotes]
 }
 
-function footnotesContent(footnotes) {
-    const fns = footnotes.map((fn, idx) => {
-        const i = idx + 1
-        const text = richText(fn)
-        return wrap('note', text, {n: i, rend: 'footnote text', 'xml:id': `ftn${i}`})
-    }).join('')
-    return wrap('div', fns, {type: 'notes'})
+function bibliography(bibliography) {
+    return bibliography.content.map((item) => {
+        return wrap('bibl', extractTextNodes(item).map(n => linkify(text(n))).join(''))
+    }).join('\n')
 }
 
 
@@ -210,7 +218,7 @@ function footnotesContent(footnotes) {
  * the document and the documents content object and generates a string
  * of TEI XML suitable for download.
  */
-function convert(slug, docContents, bibDB, imgDB) {
+function convert(slug, docContents, imgDB, citationsExporter) {
     const fields = extract(docContents)
 
     // All the fields used in the TEI header:
@@ -222,16 +230,17 @@ function convert(slug, docContents, bibDB, imgDB) {
     const TEIheader = header(authorsTEI, title, date, keywordsTEI, subtitle)
 
     // All the fields used in the TEI body:
-    const text = richText(fields.richText, imgDB)
+    const result = convertBody(fields.richText, imgDB, citationsExporter.citationTexts)
+    const text = result[0]
     const TEIbody = body(text)
 
     // All the fields used in the TEI back:
-    const footnotes = footnotesContent(fields.footnotes)
-    const bib = bibliography(bibDB)
-    const TEIback = back(footnotes, bib)
+    const footnotes = result[1]
+    const bibItems = bibliography(citationsExporter.bibliography)
+    const TEIback = back(footnotes, citationsExporter.bibliographyHeader, bibItems)
 
     return TEITemplate(slug, TEIheader, TEIbody, TEIback)
 }
 
-export {authors, richText, text, footnotesContent}
+export {authors, convertBody, text}
 export default convert

fiduswriter/dhdconf/static/js/modules/exporter/tei/convert.test.js
@@ -1,8 +1,7 @@
 import {
     authors,
-    richText,
-    text,
-    footnotesContent
+    convertBody,
+    text
 } from './convert'
 
 
@@ -51,21 +50,21 @@ test('render bold text', () => {
 })
 
 test('render single paragraph', () => {
-    expect(richText([{
+    expect(convertBody([{
         type: 'paragraph',
         content: [{type: 'text', text: 'hello'}],
     }])).toBe('<p>hello</p>')
 })
 
 test('paragraph without content should just be a line break', () => {
-    expect(richText([{
+    expect(convertBody([{
         type: 'paragraph',
         attrs: {track: []}
     }])).toBe('<lb />')
 })
 
 test('render paragraph with multiple children', () => {
-    expect(richText([{
+    expect(convertBody([{
         type: 'paragraph',
         content: [{type: 'text', text: 'hello '}, {type: 'text', text: 'world', marks: [{type: 'em'}]}],
     }])).toBe('<p>hello <hi rend="italic">world</hi></p>')
@@ -73,7 +72,7 @@ test('render paragraph with multiple children', () => {
 
 test('render heading with a single piece of text', () => {
     expect(
-        richText([{
+        convertBody([{
             type: 'heading1',
             content: [{type: 'text', text: 'hello'}],
         }])
@@ -82,7 +81,7 @@ test('render heading with a single piece of text', () => {
 
 test('render heading with italic text', () => {
     expect(
-        richText([
+        convertBody([
             {
                 type: 'heading1',
                 content: [
@@ -96,7 +95,7 @@ test('render heading with italic text', () => {
 
 test('render heading and following paragraph', () => {
     expect(
-        richText([
+        convertBody([
             {
                 type: 'heading1',
                 content: [{type: 'text', text: 'hello'}],
@@ -111,7 +110,7 @@ test('render heading and following paragraph', () => {
 
 test('render second order heading', () => {
     expect(
-        richText([{
+        convertBody([{
             type: 'heading2',
             content: [{type: 'text', text: 'hello'}],
         }])
@@ -120,7 +119,7 @@ test('render second order heading', () => {
 
 test('render two headings', () => {
     expect(
-        richText([
+        convertBody([
             {
                 type: 'heading1',
                 content: [{type: 'text', text: 'first'}],
@@ -138,7 +137,7 @@ test('render two headings', () => {
 
 test('render consecutive headings', () => {
     expect(
-        richText([
+        convertBody([
             {type: 'heading1', content: [{type: 'text', text: 'one'}]},
             {type: 'heading1', content: [{type: 'text', text: 'another'}]},
         ])
@@ -150,7 +149,7 @@ test('render consecutive headings', () => {
 
 test('render footnotes (inside the main text)', () => {
     expect(
-        richText([{
+        convertBody([{
             type: 'footnote',
             attrs: {
                 footnote: [
@@ -163,7 +162,7 @@ test('render footnotes (inside the main text)', () => {
 
 test('consecutive footnotes (inside the main text) must be numbered', () => {
     expect(
-        richText([
+        convertBody([
             {
                 type: 'footnote',
                 attrs: {
@@ -184,27 +183,9 @@ test('consecutive footnotes (inside the main text) must be numbered', () => {
     ).toBe('<ref n="1" target="ftn1" /><ref n="2" target="ftn2" />')
 })
 
-test('render list of footnotes at the bottom of the document', () => {
-    expect(
-        footnotesContent([
-            [
-                {type: 'paragraph', content: [{type: 'text', text: 'note of the foot'}]},
-            ],
-            [
-                {type: 'paragraph', content: [{type: 'text', text: 'foot of the note'}]},
-            ],
-        ])
-    ).toBe(
-        '<div type="notes">' +
-    '<note n="1" rend="footnote text" xml:id="ftn1"><p>note of the foot</p></note>' +
-    '<note n="2" rend="footnote text" xml:id="ftn2"><p>foot of the note</p></note>' +
-    '</div>'
-    )
-})
-
 test('render equations', () => {
     expect(
-        richText([
+        convertBody([
             {
                 type: 'equation',
                 attrs: {
@@ -230,7 +211,7 @@ test('render a simple figure', () => {
     ]
     const imgDB = {db: {1: {image: '/media/images/stuff.png'}}}
     expect(
-        richText(content, imgDB)
+        convertBody(content, imgDB)
     ).toBe('<figure><graphic url="stuff.png" /><head>Abbildung 1</head></figure>')
 })
 
@@ -258,7 +239,7 @@ test('render a figure with caption', () => {
     ]
     const imgDB = {db: {1: {image: '/media/images/stuff.png'}}}
     expect(
-        richText(content, imgDB)
+        convertBody(content, imgDB)
     ).toBe('<figure><graphic url="stuff.png" /><head>Abbildung 1: a caption</head></figure>')
 })
 
@@ -293,7 +274,7 @@ test('render a simple table', () => {
                    '<row><cell><p>1</p></cell><cell><p>2</p></cell></row>' +
                    '<row><cell><p>3</p></cell><cell><p>4</p></cell></row>' +
                    '</table>'
-    expect(richText(content)).toBe(expected)
+    expect(convertBody(content)).toBe(expected)
 })
 
 test('render a table with caption', () => {
@@ -330,7 +311,7 @@ test('render a table with caption', () => {
                    '<row><cell><p>1</p></cell><cell><p>2</p></cell></row>' +
                    '<row><cell><p>3</p></cell><cell><p>4</p></cell></row>' +
                    '</table>'
-    expect(richText(content)).toBe(expected)
+    expect(convertBody(content)).toBe(expected)
 })
 
 test('render a table with header row', () => {
@@ -364,7 +345,7 @@ test('render a table with header row', () => {
                    '<row role="label"><cell role="label"><p>1</p></cell><cell role="label"><p>2</p></cell></row>' +
                    '<row><cell><p>3</p></cell><cell><p>4</p></cell></row>' +
                    '</table>'
-    expect(richText(content)).toBe(expected)
+    expect(convertBody(content)).toBe(expected)
 })
 
 test('render a blockquote', () => {
@@ -385,7 +366,7 @@ test('render a blockquote', () => {
         }
     ]
     const expected = '<quote><p>a block quote.</p></quote>'
-    expect(richText(content)).toBe(expected)
+    expect(convertBody(content)).toBe(expected)
 })
 
 test('render source code snippets', () => {
@@ -405,7 +386,7 @@ test('render source code snippets', () => {
     ]
     const expected = '<code># source code\ndef random():\n    '
                  + '# determined by fair dice roll\n    return 3</code>'
-    expect(richText(content)).toBe(expected)
+    expect(convertBody(content)).toBe(expected)
 })
 
 test('render a simple unordered list', () => {
@@ -448,7 +429,7 @@ test('render a simple unordered list', () => {
                  + '<item><p>ein Listenpunkt</p></item>'
                  + '<item><p>noch einer</p></item>'
                  + '</list>'
-    expect(richText(content)).toBe(expected)
+    expect(convertBody(content)).toBe(expected)
 })
 
 test('render a simple ordered list', () => {
@@ -491,5 +472,5 @@ test('render a simple ordered list', () => {
                  + '<item><p>erster Listenpunkt.</p></item>'
                  + '<item><p>zweiter Listenpunkt</p></item>'
                  + '</list>'
-    expect(richText(content)).toBe(expected)
+    expect(convertBody(content)).toBe(expected)
 })

fiduswriter/dhdconf/static/js/modules/exporter/tei/bibliography.js
@@ -1,58 +1,0 @@
-import {wrap} from './utils'
-import {text} from './convert'
-
-
-function name(pers) {
-    return wrap('name',
-        wrap('surname',
-            pers.family.map(text).join('')) + wrap('forename', pers.given.map(text).join(''))
-    )
-}
-
-
-function biblItem(item) {
-    const f = item.fields
-    const authors = f.author?.map(a => wrap('author', name(a))).join('') || ''
-    let title = f.title?.filter(it => it.type === 'text')
-        .map(t => text(t))
-        .join('')
-    title = title ? wrap('title', title) : ''
-    const date = f.date ? wrap('date', f.date, {when: f.date}) : ''
-    const doi = f.doi ? wrap('idno', f.doi, {type: 'DOI'}) : ''
-    const url = f.url ? wrap('ref', f.url, {target: f.url}) : ''
-    const isbn = f.isbn ? wrap('idno', f.isbn.map(t => text(t)).join(''), {type: 'ISBN'}) : ''
-    const issn = f.issn ? wrap('idno', f.issn.map(t => text(t)).join(''), {type: 'ISSN'}) : ''
-    const journaltitle = f.journaltitle
-        ? wrap('title', f.journaltitle.map(t => text(t)).join(''), {level: 'j'})
-        : ''
-    const issue = f.issue
-        ? wrap('biblScope', f.issue.map(t => text(t)).join(''), {unit: 'issue'})
-        : ''
-    let editors = f.editor?.map(pers => wrap('editor', name(pers))).join('') || ''
-    editors += f.editora?.map(pers => wrap('editor', name(pers))).join('') || ''
-    let publisher = f.publisher?.map(p => {
-        return p.filter(it => it.type === 'text')
-            .map(t => text(t))
-            .join('')
-    }).join('')
-    publisher = publisher ? wrap('publisher', publisher) : ''
-    const edition = f.edition ? wrap('edition', f.edition.map(t => text(t)).join('')) : ''
-    const place = f.location
-        ? wrap('pubPlace', f.location.map(a => a.map(t => text(t)).join('')).join(''))
-        : ''
-    const content = [authors, title, date, doi, url, isbn, issn, journaltitle, issue,
-        edition, editors, publisher, place].filter(c => c !== '').join('\n')
-    return content ? wrap('bibl', content) : ''
-}
-
-function bibliography(bibDB) {
-    const head = wrap('head', 'Bibliographie')
-    const items = Object.values(bibDB.db).map(item => biblItem(item)).join('\n')
-    return wrap('div',
-        wrap('listBibl', `${head}\n${items}`),
-        {type: 'bibliography'}
-    )
-}
-
-export {name, biblItem}
-export default bibliography

commit fda10ed8ef31218c8d07d35e7e1da54533315ac8

Fix failing tests result from new refrence/footnote handling caused by 22988c1

fiduswriter/dhdconf/static/js/modules/exporter/tei/convert.js
@@ -208,7 +208,9 @@ function convertBody(richTextContent, imgDB, citationTexts) {
     const closing = '</div>'.repeat(divLevel)
     const body = `${result}${closing}`
 
-    const footnotes = wrap('div', footnotesTEI.join("\n"), {type: 'notes'})
+    const footnotes = footnotesTEI.length
+        ? wrap('div', footnotesTEI.join("\n"), {type: 'notes'})
+        : '';
     return [body, footnotes]
 }
 

fiduswriter/dhdconf/static/js/modules/exporter/tei/convert.test.js
@@ -53,21 +53,21 @@ test('render single paragraph', () => {
     expect(convertBody([{
         type: 'paragraph',
         content: [{type: 'text', text: 'hello'}],
-    }])).toBe('<p>hello</p>')
+    }])).toStrictEqual(['<p>hello</p>', ''])
 })
 
 test('paragraph without content should just be a line break', () => {
     expect(convertBody([{
         type: 'paragraph',
         attrs: {track: []}
-    }])).toBe('<lb />')
+    }])).toStrictEqual(['<lb />', ''])
 })
 
 test('render paragraph with multiple children', () => {
     expect(convertBody([{
         type: 'paragraph',
         content: [{type: 'text', text: 'hello '}, {type: 'text', text: 'world', marks: [{type: 'em'}]}],
-    }])).toBe('<p>hello <hi rend="italic">world</hi></p>')
+    }])).toStrictEqual(['<p>hello <hi rend="italic">world</hi></p>', ''])
 })
 
 test('render heading with a single piece of text', () => {
@@ -76,7 +76,7 @@ test('render heading with a single piece of text', () => {
             type: 'heading1',
             content: [{type: 'text', text: 'hello'}],
         }])
-    ).toBe('<div rend="DH-Heading"><head>hello</head></div>')
+    ).toStrictEqual(['<div rend="DH-Heading"><head>hello</head></div>', ''])
 })
 
 test('render heading with italic text', () => {
@@ -90,7 +90,10 @@ test('render heading with italic text', () => {
                 ],
             },
         ])
-    ).toBe('<div rend="DH-Heading"><head>hello <hi rend="italic">world</hi></head></div>')
+    ).toStrictEqual([
+        '<div rend="DH-Heading"><head>hello <hi rend="italic">world</hi></head></div>',
+        ''
+    ])
 })
 
 test('render heading and following paragraph', () => {
@@ -105,7 +108,7 @@ test('render heading and following paragraph', () => {
                 content: [{type: 'text', text: 'world'}],
             },
         ])
-    ).toBe('<div rend="DH-Heading"><head>hello</head><p>world</p></div>')
+    ).toStrictEqual(['<div rend="DH-Heading"><head>hello</head><p>world</p></div>', ''])
 })
 
 test('render second order heading', () => {
@@ -114,7 +117,7 @@ test('render second order heading', () => {
             type: 'heading2',
             content: [{type: 'text', text: 'hello'}],
         }])
-    ).toBe('<div rend="DH-Heading"><head>hello</head></div></div>')
+    ).toStrictEqual(['<div rend="DH-Heading"><head>hello</head></div></div>', ''])
 })
 
 test('render two headings', () => {
@@ -129,10 +132,11 @@ test('render two headings', () => {
                 content: [{type: 'text', text: 'second'}],
             },
         ])
-    ).toBe(
+    ).toStrictEqual([
         '<div rend="DH-Heading"><head>first</head>' +
-    '<div rend="DH-Heading"><head>second</head></div></div>'
-    )
+        '<div rend="DH-Heading"><head>second</head></div></div>',
+        ''
+    ])
 })
 
 test('render consecutive headings', () => {
@@ -141,10 +145,11 @@ test('render consecutive headings', () => {
             {type: 'heading1', content: [{type: 'text', text: 'one'}]},
             {type: 'heading1', content: [{type: 'text', text: 'another'}]},
         ])
-    ).toBe(
+    ).toStrictEqual([
         '<div rend="DH-Heading"><head>one</head></div>' +
-    '<div rend="DH-Heading"><head>another</head></div>'
-    )
+        '<div rend="DH-Heading"><head>another</head></div>',
+        ''
+    ])
 })
 
 test('render footnotes (inside the main text)', () => {
@@ -157,7 +162,10 @@ test('render footnotes (inside the main text)', () => {
                 ],
             },
         }])
-    ).toBe('<ref n="1" target="ftn1" />')
+    ).toStrictEqual([
+        '<ref n="1" target="ftn1" />',
+        '<div type="notes"><note n="1" rend="footnote text" xml:id="ftn1"><p>note of the foot</p></note></div>'
+    ])
 })
 
 test('consecutive footnotes (inside the main text) must be numbered', () => {
@@ -180,7 +188,13 @@ test('consecutive footnotes (inside the main text) must be numbered', () => {
                 }
             }
         ])
-    ).toBe('<ref n="1" target="ftn1" /><ref n="2" target="ftn2" />')
+    ).toStrictEqual([
+        '<ref n="1" target="ftn1" /><ref n="2" target="ftn2" />',
+        '<div type=\"notes\">' +
+            '<note n=\"1\" rend=\"footnote text\" xml:id=\"ftn1\"><p>note of the foot</p></note>\n' +
+            '<note n=\"2\" rend=\"footnote text\" xml:id=\"ftn2\"><p>foot of the note</p></note>' +
+        '</div>'
+    ])
 })
 
 test('render equations', () => {
@@ -193,7 +207,7 @@ test('render equations', () => {
                 }
             }
         ])
-    ).toBe('<formula notation="tex">$A=\\pi\\cdot r^2$</formula>')
+    ).toStrictEqual(['<formula notation="tex">$A=\\pi\\cdot r^2$</formula>', ''])
 })
 
 test('render a simple figure', () => {
@@ -212,7 +226,10 @@ test('render a simple figure', () => {
     const imgDB = {db: {1: {image: '/media/images/stuff.png'}}}
     expect(
         convertBody(content, imgDB)
-    ).toBe('<figure><graphic url="images/stuff.png" /><head>Abbildung 1</head></figure>')
+    ).toStrictEqual([
+        '<figure><graphic url="images/stuff.png" /><head>Abbildung 1</head></figure>',
+        ''
+    ])
 })
 
 test('render a figure with caption', () => {
@@ -240,7 +257,10 @@ test('render a figure with caption', () => {
     const imgDB = {db: {1: {image: '/media/images/stuff.png'}}}
     expect(
         convertBody(content, imgDB)
-    ).toBe('<figure><graphic url="images/stuff.png" /><head>Abbildung 1: a caption</head></figure>')
+    ).toStrictEqual([
+        '<figure><graphic url="images/stuff.png" /><head>Abbildung 1: a caption</head></figure>',
+        ''
+    ])
 })
 
 test('render a simple table', () => {
@@ -274,7 +294,7 @@ test('render a simple table', () => {
                    '<row><cell><p>1</p></cell><cell><p>2</p></cell></row>' +
                    '<row><cell><p>3</p></cell><cell><p>4</p></cell></row>' +
                    '</table>'
-    expect(convertBody(content)).toBe(expected)
+    expect(convertBody(content)).toStrictEqual([expected, ''])
 })
 
 test('render a table with caption', () => {
@@ -311,7 +331,7 @@ test('render a table with caption', () => {
                    '<row><cell><p>1</p></cell><cell><p>2</p></cell></row>' +
                    '<row><cell><p>3</p></cell><cell><p>4</p></cell></row>' +
                    '</table>'
-    expect(convertBody(content)).toBe(expected)
+    expect(convertBody(content)).toStrictEqual([expected, ''])
 })
 
 test('render a table with header row', () => {
@@ -345,7 +365,7 @@ test('render a table with header row', () => {
                    '<row role="label"><cell role="label"><p>1</p></cell><cell role="label"><p>2</p></cell></row>' +
                    '<row><cell><p>3</p></cell><cell><p>4</p></cell></row>' +
                    '</table>'
-    expect(convertBody(content)).toBe(expected)
+    expect(convertBody(content)).toStrictEqual([expected, ''])
 })
 
 test('render a blockquote', () => {
@@ -366,7 +386,7 @@ test('render a blockquote', () => {
         }
     ]
     const expected = '<quote><p>a block quote.</p></quote>'
-    expect(convertBody(content)).toBe(expected)
+    expect(convertBody(content)).toStrictEqual([expected, ''])
 })
 
 test('render source code snippets', () => {
@@ -386,7 +406,7 @@ test('render source code snippets', () => {
     ]
     const expected = '<code># source code\ndef random():\n    '
                  + '# determined by fair dice roll\n    return 3</code>'
-    expect(convertBody(content)).toBe(expected)
+    expect(convertBody(content)).toStrictEqual([expected, ''])
 })
 
 test('render a simple unordered list', () => {
@@ -429,7 +449,7 @@ test('render a simple unordered list', () => {
                  + '<item><p>ein Listenpunkt</p></item>'
                  + '<item><p>noch einer</p></item>'
                  + '</list>'
-    expect(convertBody(content)).toBe(expected)
+    expect(convertBody(content)).toStrictEqual([expected, ''])
 })
 
 test('render a simple ordered list', () => {
@@ -472,5 +492,5 @@ test('render a simple ordered list', () => {
                  + '<item><p>erster Listenpunkt.</p></item>'
                  + '<item><p>zweiter Listenpunkt</p></item>'
                  + '</list>'
-    expect(convertBody(content)).toBe(expected)
+    expect(convertBody(content)).toStrictEqual([expected, ''])
 })