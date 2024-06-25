1a1cead9dbf1d71111d96d45eee78c20a0af2c18
David Neugebauer <david.neugebauer@posteo.de>
2024-06-25 17:58:07 +0200

    Only allow text in the abstract section

--- fiduswriter/dhdconf/settings.py
+++ fiduswriter/dhdconf/settings.py
@@ -21,18 +21,6 @@ DHD_ARTICLE_ATTRS = {
 
 DHD_ARTICLE_ABSTRACT_ELEMENTS = [
     "paragraph",
-    "heading1",
-    "heading2",
-    "heading3",
-    "figure",
-    "ordered_list",
-    "bullet_list",
-    "equation",
-    "citation",
-    "cross_reference",
-    "blockquote",
-    "footnote",
-    "table"
 ]
 
 DHD_ARTICLE_BODY_ELEMENTS = [
