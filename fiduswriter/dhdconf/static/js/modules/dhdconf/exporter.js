861cbdc0ab971beb36c69abb19c80b8d245cb4e8
Philip Schildkamp <philip.schildkamp@uni-koeln.de>
2026-08-18 16:45:44 +0200

    Minor style normalisations

--- fiduswriter/dhdconf/static/js/modules/dhdconf/exporter.js
+++ fiduswriter/dhdconf/static/js/modules/dhdconf/exporter.js
@@ -4,7 +4,6 @@ import {config} from "./config";
 
 
 export class DhdConfHtmlExporter extends HTMLExporter {
-
     init() {
         this.styleSheets.push({url: staticUrl("css/dhdconf_export_html.css")})
         return super.init()
