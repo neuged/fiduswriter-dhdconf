c96e7f734dd095aa58b7e45af53edc77cb597bf9
Philip Schildkamp <philip.schildkamp@uni-koeln.de>
2026-08-21 14:03:47 +0200

    Block requests to allauth routes and disable password reset

--- fiduswriter/dhdconf/middleware.py
+++ fiduswriter/dhdconf/middleware.py
@@ -2,6 +2,7 @@ import django.conf
 from django.http import HttpResponseForbidden
 from django.urls import reverse
 
+import allauth.account.views
 import bibliography.views
 import document.views
 import user.views
@@ -38,16 +39,34 @@ class RequestBlockingMiddleware:
             user.views.invite,
             user.views.invites_accept,
             user.views.invites_decline,
+        ],
+        "BLOCK_ALLAUTH_ROUTES": [
+            allauth.account.views.email,
+            allauth.account.views.password_reset,
+            allauth.account.views.signup,
         ]
     }
+
+    RAW_PATHS = {
+        "BLOCK_USER_CHANGES": [
+            "/api/user/password/reset/"
+        ]
+    }
+
 
     def __init__(self, get_response):
         self.get_response = get_response
         self.blocked = []
+
         for setting, paths in self.PATHS.items():
             if getattr(django.conf.settings, setting, False):
                 for path in paths:
                     self.blocked.append(reverse(path))
+
+        for setting, raw_paths in self.RAW_PATHS.items():
+            if getattr(django.conf.settings, setting, False):
+                for raw_path in raw_paths:
+                    self.blocked.append(raw_path)
 
     def __call__(self, request):
         if request.method == "POST" and request.path_info in self.blocked:
